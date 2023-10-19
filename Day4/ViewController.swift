cb358287f3624e03dcb1725338dbcfd04ad0300d
Kevin Sagita <kevinsagita79@gmail.com>
2023-10-19 10:32:23 +0700

    Initial Commit

---
+++ Day4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day4
+//
+//  Created by P090MMCTSE011 on 19/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Day4UITests/Day4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Day4UITestsLaunchTests.swift
+//  Day4UITests
+//
+//  Created by P090MMCTSE011 on 19/10/23.
+//
+
+import XCTest
+
+final class Day4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
